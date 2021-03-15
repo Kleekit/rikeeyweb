38811f4f4b08bb876bb7ec880db7ed9caf29b6de
Amayo Collins <77975763+alsoknownaszac@users.noreply.github.com>
2021-03-15 17:39:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+var path = require('path');
+const app = express();
+const port = 7000;
+// view engine setup
+ 
+app.set('view engine', 'ejs');
+//setup public folder
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/asset'));
+
+ 
+
+app.get('/',function (req, res) {
+    res.locals.title = "RIKEEY'S HOME";  
+    res.render('landing')
+});
+
+
+
+
+app.listen(port, () => console.log(`MasterEJS app Started on port ${port}!`));
